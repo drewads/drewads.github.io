4798fac8460502e1c251cd21c6d60a09291bc417
Drew Wadsworth <drew.wadsworth@gmail.com>
2017-01-22 20:54:21 +0000

    daily rotating pages

--- background.js
+++ background.js
@@ -8,30 +8,30 @@ function setBackground()
   var dateAsString = currentDate.toString();
   if (dateAsString.substring(0, 1) == "M")
   {
-    document.body.style.backgroundImage = "url('img_F9.jpg')";
+    document.body.style.backgroundImage = "url('img_hull.jpg')";
   }
   else if (dateAsString.substring(0, 2) == "Tu")
   {
-     
+     $('body').css('background-image', 'url(img_crs3.jpg)');
   }
   else if (dateAsString.substring(0, 1) == "W")
   {
-    
+    $('body').css('background-image', 'url(img_blockIsland.jpg)');
   }
   else if (dateAsString.substring(0, 2) == "Th")
   {
-     
+    $('body').css('background-image', 'url(img_exposureLeft.jpg)');
   }
   else if (dateAsString.substring(0, 1) == "F")
   {
-     
+     $('body').css('background-image', 'url(img_sunriseLakes.jpg)');
   }
   else if (dateAsString.substring(0, 2) == "Sa")
   {
-     $('body').css('background-image', 'url(img_F9.jpg)');
+     $('body').css('background-image', 'url(img_lakeChutes.jpg)');
   }
   else if (dateAsString.substring(0, 2) == "Su")
   {
-     $('body').css('background-image', 'url(img_launchExposure.jpg)');
+     $('body').css('background-image', 'url(img_exposureRight.jpg)');
   }
 }  
